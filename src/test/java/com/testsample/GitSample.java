44379b01c3c7bc6e2abe20208a03e231fb4b5e25
TamilMechian <tamizhspa143@gmail.com>
2022-06-19 11:08:50 +0530

    test sample

---
+++ src/test/java/com/testsample/GitSample.java
@@ -0,0 +1,13 @@
+package com.testsample;
+
+public class GitSample {
+	
+	private void method1() {
+System.out.println("tc01");
+	}
+	private void method2() {
+System.out.println("tc02");
+	}
+	
+
+}
